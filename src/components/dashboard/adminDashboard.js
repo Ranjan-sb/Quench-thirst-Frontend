b00aa9af38f2cf48beadcc8e2856ac762c308619
Adarsh A Koushik <adarshkoushik2000@gmail.com>
2024-04-23 11:05:36 +0530

    added supplierTable code

--- src/components/dashboard/adminDashboard.js
+++ src/components/dashboard/adminDashboard.js
@@ -1,7 +1,63 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+import SuppliersTable from "../pages/suppliersTable"
+
 export default function AdminDashboard(){
+    const [suppliers, setSuppliers] = useState([]);
+    useEffect(()=>{
+        (async()=>{
+            const response = await axios.get('http://localhost:3100/api/suppliers',{
+                headers : {
+                    Authorization : localStorage.getItem('token')
+                }
+            })
+            console.log(response.data)
+            setSuppliers(response.data)
+        })()
+    },[])
+    
+    const handleApprove = async (supplierId) => {
+        try {
+            const response = await axios.put(`http://localhost:3100/api/suppliers/${supplierId}/approve`,{
+                headers : {
+                    Authorization : localStorage.getItem('token')
+                }
+            })
+            setSuppliers(prevSuppliers => prevSuppliers.map(supplier => {
+                if (supplier._id === supplierId) {
+                    return { ...supplier, isApproved: true };
+                }
+                return supplier;
+            }));
+            console.log('Supplier approved successfully:', response.data);
+        } catch (error) {
+            console.error('Error approving supplier:', error);
+        }
+    }
+
+    const handleRemove = async (supplierId) => {
+        try {
+            const confirmation = window.confirm('Are you sure you want to delete ?')
+            if(confirmation){
+                const response = await axios.delete(`http://localhost:3100/api/suppliers/${supplierId}`,{
+                    headers : {
+                        Authorization : localStorage.getItem('token')
+                    }
+                })
+                setSuppliers(prevSuppliers => prevSuppliers.filter(supplier => supplier._id !== supplierId));
+                console.log('Supplier removed successfully:', response.data);
+            }
+           
+        } catch (error) {
+            console.error('Error removing supplier:', error);
+        }
+    };
     return (
         <div>
             <h3>Admin Dashboard</h3>
+            <h3>Suppliers List - {suppliers.length}</h3>
+            <SuppliersTable suppliers={suppliers} handleApprove={handleApprove} handleRemove={handleRemove} />
         </div>
     )
 }
