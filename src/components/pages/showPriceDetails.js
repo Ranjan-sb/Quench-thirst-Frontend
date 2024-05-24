8e870d04da1e7ef88b01d68ddff4adc9d53310df
Ranjan-sb <165002110+Ranjan-sb@users.noreply.github.com>
2024-05-24 18:23:09 +0530

    Added Delete functionality
    Added Delete functionality for admin in showPriceDetails

--- src/components/pages/showPriceDetails.js
+++ src/components/pages/showPriceDetails.js
@@ -1,8 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { VehicleTypeContext } from "../../context/VehicleTypeContext";
+import { useAuth } from "../../context/AuthContext";
+import { startRemoveVehicleType, startGetVehicleTypes } from "../../actions/vehicleType-action";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
 
 export default function ShowPriceDetails() {
   const { vehicleTypes } = useContext(VehicleTypeContext);
+  const {user}=useAuth()
+  console.log("knowUser-", user)
+
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+      dispatch(startGetVehicleTypes)
+  },[dispatch])
+
+  // const handleDelete=(id)=>{
+  //   if (window.confirm("Are you sure you want to delete this vehicle type?")) {
+  //     dispatch(startRemoveVehicleType(id));
+  //   }
+  // }
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(startRemoveVehicleType(id));
+        Swal.fire(
+          "Deleted!",
+          "The vehicle type has been deleted.",
+          "success"
+        );
+      }
+    });
+  };
 
   return (
     <div className="row justify-content-center">
@@ -36,6 +74,16 @@ export default function ShowPriceDetails() {
                     ))}
                   </ul>
                 </td>
+                {user && user.role==='admin' && (
+                  <td>
+                    <button 
+                      onClick={()=>handleDelete(ele._id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                )}
               </tr>
             ))}
           </tbody>
